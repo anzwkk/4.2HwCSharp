7dc26714866387c56ae1b428e36f5a6f6a6864a2
anzwkk <anam89737@gmail.com>
2024-12-28 01:57:38 +0300

    Added calculator with +, -, *, / operations via switch

---
+++ Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int firstNum;
+        int secondNum;
+        int result = 0;
+        char mathAction;
+
+        Console.WriteLine("vvedite pervoe chislo: ");
+        firstNum = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("vvedite vtoroe chislo: ");
+        secondNum = Convert.ToInt32(Console.ReadLine());
+
+        do
+        {
+                Console.WriteLine(" +,-,*,/ ");
+                mathAction = Convert.ToChar(Console.ReadLine());
+                switch (mathAction)
+                {
+
+                    case '+':
+                        result = firstNum + secondNum;
+                        break;
+                    case '-':
+                        result = firstNum - secondNum;
+                        break;
+                    case '*':
+                        result = firstNum * secondNum;
+                        break;
+                    case '/':
+                        result = firstNum / secondNum;
+                        break;
+                    default:
+                        Console.WriteLine("Nekorektna matem diya");
+                }
+            Console.WriteLine($"Resultat {firstNum} {secondNum} = {result}");
+            } while (true) ;
+    }
+}
